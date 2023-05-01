34b2d32278913b2038ba9e9055ce22ffe0d941e9
Jimmy <jimmy@thiknpad.com>
2023-05-01 04:22:27 +0300

    refactor: clang-format and comments remove

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,26 +2,20 @@
 
 #include <JuceHeader.h>
 
-//==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
-{
-public:
-    //==============================================================================
+class MainComponent : public juce::Component {
+   public:
     MainComponent();
     ~MainComponent() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
 
-private:
-    //==============================================================================
+   private:
     // Your private member variables go here...
 
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
